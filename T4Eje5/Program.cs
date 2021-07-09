5dcdbec3f6ac755f65a95ae7a2d3b9bbf6da8354
caroldbt <37454302+caroldbt@users.noreply.github.com>
2021-07-09 22:34:59 +0200

    Update Program.cs

--- T4Eje5/Program.cs
+++ T4Eje5/Program.cs
@@ -6,7 +6,28 @@ namespace T4Eje5
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*************
+             * Ejercicio 5
+             * ***********/
+            Console.WriteLine("Ingrese el nombre de la semana sin acentos: ");
+            string semana = Console.ReadLine().ToLower();
+            switch (semana)
+            {
+                case "lunes":
+                case "martes":
+                case "miercoles":
+                case "jueves":
+                case "viernes":
+                    Console.WriteLine("No es fin de semana.");
+                    break;
+                case "sabado":
+                case "domingo":
+                    Console.WriteLine("Es fin de semana");
+                    break;
+                default:
+                    Console.WriteLine("Dia de la semana no existe");
+                    break;
+
         }
     }
 }
